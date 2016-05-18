45e357eb84f01d821152714ff3c5c6acd40d5c86
Sam Thomas <sgpthomas@gmail.com>
2016-05-17 18:51:38 -0700

    added some fancy ass scolling

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,15 +1,17 @@
 // SMOOTH SCROLLING SECTIONS
-$('a[href*=#]:not([href=#])').click(function() {
+$(document).ready(function() {
+  $('a[href*=\\#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 800);
+       return false;
+      }
     }
+  });
 });
