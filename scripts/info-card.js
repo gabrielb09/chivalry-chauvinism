148e415b97cdbf9d3290801bec3ab489d9924d5b
Samuel Thomas <sgpthomas@gmail.com>
2016-05-19 09:35:34 -0700

    Update info-card.js

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -1,7 +1,7 @@
 var sel = "section .subitem a";
 $(document).ready(function() {
   $(sel).click(function() {
-    console.log("click");
+    console.log("clicked");
     var classList = $(sel).attr('class').split(/\s+/);
     $.each(classList, function(index, item) {
         console.log(item);
