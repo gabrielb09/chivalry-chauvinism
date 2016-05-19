c4ec69f991daa307a3a5c156ba830806dab3776b
Samuel Thomas <sgpthomas@gmail.com>
2016-05-19 09:54:06 -0700

    Update info-card.js

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Hide everything
-  var classList = $("card").attr('class').split(/\s+/);
+  var classList = $(".card").attr('class').split(/\s+/);
   $.each(classList, function(index, item) {
     if (item.indexOf('-') != -1) { //has a dash in the class
       $(item).css("visibility", "hidden");
