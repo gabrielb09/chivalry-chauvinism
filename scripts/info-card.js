3ccf730231a85adf922eed253a1f1ab81a57d833
Samuel Thomas <sgpthomas@gmail.com>
2016-05-19 09:43:15 -0700

    Update info-card.js

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
     console.log("click");
     var classList = $("section .subitem").attr('class').split(/\s+/);
     $.each(classList, function(index, item) {
-        console.log(item);
+        if("card" in item) {
+          console.log(item);
+          var cardList = item.split("-");
+          var cardId = parseInt(cardList[1]);
+          console.log(cardId.toString());
+        }
     });
   });
 });
