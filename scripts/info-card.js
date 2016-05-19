95b8787f26a2d36aec22fd4cbcc890478cc82827
Samuel Thomas <sgpthomas@gmail.com>
2016-05-19 09:53:33 -0700

    Update info-card.js

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
+  // Hide everything
+  var classList = $("card").attr('class').split(/\s+/);
+  $.each(classList, function(index, item) {
+    if (item.indexOf('-') != -1) { //has a dash in the class
+      $(item).css("visibility", "hidden");
+    }
+  });
+  
   $("section .subitem a").click(function() {
     console.log("click");
     var classList = $("section .subitem").attr('class').split(/\s+/);
     $.each(classList, function(index, item) {
-        if("card" in item) {
-          console.log(item);
-          var cardList = item.split("-");
-          var cardId = parseInt(cardList[1]);
-          console.log(cardId.toString());
-        }
+        
     });
   });
 });
