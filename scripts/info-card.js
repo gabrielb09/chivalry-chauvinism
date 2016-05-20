e8b29bf9c7a9b31dda32a31a83b3f42535bc5a35
Sam Thomas <sgpthomas@gmail.com>
2016-05-20 07:28:03 -0700

    some stuffs

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
-  // Hide everything
-  var classList = $(".card").attr('class').split(/\s+/);
-  $.each(classList, function(index, item) {
-    if (item.indexOf('-') != -1 && item.indexOf('card') != -1) { //has a dash in the class
-      // $(item).css("visibility", "hidden");
-      console.log(item);
+
+  // listen for escape key
+  $(window).keyup(function(e) {
+    var key = (e.keyCode ? e.keyCode : e.which);
+    if (key == 27) {
+      hideAll();
     }
   });
-  
+
   $("section .subitem a").click(function() {
     console.log("click");
     var classList = $("section .subitem").attr('class').split(/\s+/);
     $.each(classList, function(index, item) {
-        
+
     });
     $(".card.card-2").css("visibility", "visible");
   });
 });
+
+function hideAll() {
+  // Hide everything
+  var classList = $(".card").attr('class').split(/\s+/);
+  $.each(classList, function(index, item) {
+    if (item.indexOf('-') != -1 && item.indexOf('card') != -1) { //has a dash in the class
+      $(".card." + item).css("visibility", "hidden");
+      // $(item).hide();
+      console.log(item);
+    }
+  });
+}
