f2cb3d9dd79f45c9f9e27ee52aa58d3133739395
Sam Thomas <sgthomas@gmail.com>
2016-05-20 14:56:03 -0700

    added some fancy animations

--- scripts/info-card.js
+++ scripts/info-card.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
+  hideAll();
 
   // listen for escape key
   $(window).keyup(function(e) {
     var key = (e.keyCode ? e.keyCode : e.which);
     if (key == 27) {
+      console.log("Close Popups");
       hideAll();
     }
   });
 
   $("section .subitem a").click(function() {
-    console.log("click");
-    var classList = $("section .subitem").attr('class').split(/\s+/);
-    $.each(classList, function(index, item) {
-
-    });
-    $(".card.card-2").css("visibility", "visible");
+    var classList = $(this).parent().attr('class').split(/\s+/);
+    $(".card." + classList[2] + "." + classList[1]).addClass("cardpop");
   });
 });
 
 function hideAll() {
   // Hide everything
   var classList = $(".card").attr('class').split(/\s+/);
-  $.each(classList, function(index, item) {
-    if (item.indexOf('-') != -1 && item.indexOf('card') != -1) { //has a dash in the class
-      $(".card." + item).css("visibility", "hidden");
-      // $(item).hide();
-      console.log(item);
-    }
+  $.each($('.card'), function(index, item) {
+    $(item).removeClass("cardpop");
   });
+  // $.each(classList, function(index, item) {
+  //   if (item.indexOf('-') != -1 && item.indexOf('card') != -1) { //has a dash in the class
+  //     $(".card." + item).removeClass("cardpop");
+  //     console.log(".card." + item);
+  //     // $(item).hide();
+  //     // console.log(item);
+  //   }
+  // });
 }
